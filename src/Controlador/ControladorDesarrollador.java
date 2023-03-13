a90855abd172b48ce76ca4dd2836417943411798
abdelrz <102103267+abdelrz@users.noreply.github.com>
2023-03-13 14:38:56 -0500

    Add files via upload

---
+++ src/Controlador/ControladorDesarrollador.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Vista.*;
+import Vista.Ayuda.*;
+import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author abdel
+ */
+public class ControladorDesarrollador {
+
+    private VistaMenuPrincipal vistaMenuPrincipal;
+    private VistaDesarrollador vistaDesarrollador;
+
+    public ControladorDesarrollador(VistaMenuPrincipal vistaMenuPrincipal) {
+        this.vistaMenuPrincipal = vistaMenuPrincipal;
+        JMenuItem vistaDesarrollador = vistaMenuPrincipal.menuDesarrollador;
+        vistaDesarrollador.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mostrarVistaDesarrollador();
+            }
+        });
+    }
+
+    public ControladorDesarrollador(VistaDesarrollador vistaDesarrollador) {
+        this.vistaDesarrollador = vistaDesarrollador;
+        asignarEventosABotones();
+    }
+
+    private void mostrarVistaDesarrollador() {
+        VistaDesarrollador desarrollador = new VistaDesarrollador();
+        desarrollador.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        desarrollador.setLocationRelativeTo(null);
+        desarrollador.setVisible(true);
+        asignarEventosABotones(desarrollador);
+    }
+
+    private void asignarEventosABotones() {
+        asignarEventosABotones(this.vistaDesarrollador);
+    }
+
+    private void asignarEventosABotones(VistaDesarrollador vista) {
+        vista.botonLinkedin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                abrirURLEnNavegador("https://www.linkedin.com/in/abdel-ramirez-costa/");
+            }
+        });
+
+        vista.botonGithub.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                abrirURLEnNavegador("https://github.com/abdelrz");
+            }
+        });
+    }
+
+    private void abrirURLEnNavegador(String url) {
+        Desktop browser = Desktop.getDesktop();
+        try {
+            browser.browse(new URI(url));
+        } catch (IOException e) {
+            // Manejar excepción
+        } catch (URISyntaxException e) {
+            // Manejar excepción
+        }
+    }
+}
---
+++ src/Modelo/CalculadoraBasica.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author abdel
+ */
+public class CalculadoraBasica {
+
+    private double memoria;
+
+    public CalculadoraBasica() {
+        memoria = 0;
+
+    }
+
+    public void sumar(double numero) {
+        memoria += numero;
+    }
+
+    public void restar(double numero) {
+        memoria -= numero;
+    }
+
+    public void multiplicar(double numero) {
+        if (memoria == 0) {
+            memoria = numero;
+        } else {
+            memoria *= numero;
+        }
+    }
+
+    public void dividir(double numero) {
+        if (memoria == 0) {
+            memoria = numero;
+        } else {
+            memoria /= numero;
+        }
+    }
+
+    public void limpiar() {
+        memoria = 0;
+    }
+
+    public double getMemoria() {
+        return memoria;
+    }
+
+}
